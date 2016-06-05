96d6cda4f3e351eb0e65488fc7f549d911030752
rajasntbd <raja@sntbd.com>
2016-06-06 01:25:09 +0600

    router added

---
+++ app/dashboard.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by raja on 6/6/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-dashboard',
+    template: '<h3>My Dashboard</h3>'
+})
+export class DashboardComponent { }
